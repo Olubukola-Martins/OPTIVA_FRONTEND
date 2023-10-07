1ad2d476e799132409b60f4ceaa07d2854b23d62
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2023-10-07 23:27:30 +0100

    dashboard

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,4 @@
-// import { Navbar } from "./Navbar";
-
+import { useState } from "react";
 import { Navbar } from "./Navbar";
 import { SideBar } from "./SideBar";
 
@@ -8,13 +7,17 @@ export interface IProps {
 }
 
 export const DashboardLayout: React.FC<IProps> = ({ children }: IProps) => {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div>
       <div className="flex w-full relative">
-        <div className="w-52 fixed z-40 overflow-hidden lg:flex hidden">
-          <SideBar />
+        <div className={`w-52 fixed z-40 overflow-hidden lg:flex hidden`}>
+          <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
         </div>
-        <div className="w-full lg:ml-52 pb-10">
+        <div
+          className={isOpen ? "w-full lg:ml-52 pb-10" : "lg:ml-16 pb-10 w-full"}
+        >
           <Navbar />
           <main className="Container">{children}</main>
         </div>
--- src/features/dashboard/pages/Dashboard.tsx
+++ src/features/dashboard/pages/Dashboard.tsx
@@ -1,21 +1,59 @@
 import { PageIntro } from "src/components/PageIntro";
+import { AppButton } from "src/components/button/AppButton";
 import { SimpleCard } from "src/components/cards/SimpleCard";
 import { DashboardLayout } from "src/components/layout/Layout";
+import welcomeVector from ".././assets/welcomeVector.svg";
+import introBg from ".././assets/introBg.png";
 
 const Dashboard = () => {
   return (
     <DashboardLayout>
-      <div>
-        <PageIntro title="Dashboard" description="All descriptions here" />
+      <div className="flex items-center justify-between">
+        <PageIntro title="Dashboard" arrowBack={false} />
+        <AppButton label="Add Applicants" />
+      </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-5">
-          <SimpleCard
-            icon="ph:user-list-duotone"
-            cardColor="yellow"
-            title="card title"
-            count={9}
-          />
+      <div
+        className="flex justify-between rounded-lg Container"
+        style={{ background: `url(${introBg})` }}
+      >
+        <div className="text-white flex flex-col justify-center py-4">
+          <h2 className="font-semibold text-lg md:text-2xl pb-1">
+            Hello Ruth!
+          </h2>
+          <p className="text-sm md:text-base">Welcome to your dashboard</p>
         </div>
+        <div className="pt-5 md:flex hidden">
+          <img src={welcomeVector} alt="dashboard" />
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7 mt-7">
+        <SimpleCard
+          icon="ph:user-list-duotone"
+          cardColor="blue"
+          title="Master List"
+          count={0}
+        />
+        <SimpleCard
+          icon="ph:user-list-duotone"
+          cardColor="oxblood"
+          title="Authorized  
+            Applicants"
+          count={0}
+        />
+        <SimpleCard
+          icon="ph:user-list-duotone"
+          cardColor="green"
+          title="Paid
+          Applicants"
+          count={0}
+        />
+        <SimpleCard
+          icon="ph:user-list-duotone"
+          cardColor="yellow"
+          title="Prospects"
+          count={0}
+        />
       </div>
     </DashboardLayout>
   );
