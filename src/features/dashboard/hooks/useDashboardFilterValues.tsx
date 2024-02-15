65cc2708714d1fdafc666fe94b2c0f17d7789b1a
Olubukola <19bukola97@gmail.com>
2024-02-15 13:30:27 +0100

    Prospects filter drawer design implementation

---
+++ src/features/dashboard/hooks/useDashboardFilterValues.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useDashboardFilterValues = () => {
+  return (
+    <div>useDashboardFilterValues</div>
+  )
+}
+
+export default useDashboardFilterValues
--- src/features/dashboard/pages/Prospects.tsx
+++ src/features/dashboard/pages/Prospects.tsx
@@ -1,17 +1,30 @@
-import { Dropdown, Input, Menu, Select } from "antd";
+import { Button, Drawer, Dropdown, Form, Input, Menu, Select } from "antd";
+import { useForm } from "antd/es/form/Form";
 import Table, { ColumnsType } from "antd/es/table";
 import { useEffect, useState } from "react";
 import { PageIntro } from "src/components/PageIntro";
+import { AppButton } from "src/components/button/AppButton";
 import { END_POINT } from "src/config/environment";
-import { IAllProspectsData, IProspectDatum } from "src/features/meetings/types/types";
+import {
+  IAllProspectsData,
+  IProspectDatum,
+} from "src/features/meetings/types/types";
+import { useGetCountry } from "src/features/settings/features/program-types/hooks/useGetCountry";
+import { useGetProgramType } from "src/features/settings/features/program-types/hooks/useGetProgramType";
 import { useFetchAllItems } from "src/features/settings/hooks/useFetchAllItems";
+import { useWindowWidth } from "src/hooks/useWindowWidth";
 
 export const QUERY_KEY_FOR_PROSPECTS = "AllProspects";
 export const prospectsURL = `${END_POINT.BASE_URL}/admin/prospect-list/applicants`;
 
-
 const Prospects = () => {
+  const { data: allProgramTypes, isLoading: loadingProgramTypes } =
+    useGetProgramType();
+  const { data: countries, isLoading: loadingCountries } = useGetCountry();
   const [dataSource, setDataSource] = useState<IProspectDatum[]>();
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+  const {drawerSize } = useWindowWidth();
+  const [filterForm] = useForm();
   const {
     data,
     isLoading,
@@ -21,17 +34,6 @@ const Prospects = () => {
       urlEndPoint: prospectsURL,
     });
 
-  // TABLE DATA FOR STAGE 3
-  interface DataType {
-    key: number | string;
-    applicantID: number | string;
-    applicantName: string;
-    phoneNumber: string;
-    email: string;
-    country: string;
-    numberDependents: number;
-    onboardedBy: string;
-  }
   const columns: ColumnsType<IProspectDatum> = [
     {
       title: "SN",
@@ -93,12 +95,17 @@ const Prospects = () => {
     },
   ];
 
+  const handleFilter = (values) => {
+    
+  };
+
   useEffect(() => {
     if (data?.data) {
-  setDataSource(data.data)
-}
-  }, [isLoading,data])
-  
+      setDataSource(data.data);
+    }
+  }, [isLoading, data]);
+
+  useEffect(() => {}, [loadingCountries, loadingProgramTypes]);
 
   return (
     <>
@@ -110,7 +117,13 @@ const Prospects = () => {
       <div className="mt-6 py-4 border rounded-md border-[rgba(229, 231, 235, 1)]">
         <div className="flex gap-2 sm:gap-4 flex-col sm:flex-row sm:items-start items-center sm:pl-5">
           <Input.Search placeholder="Search" className=" w-52" />
-          <Select allowClear placeholder="Filter" className="w-52 " />
+          <Button
+            onClick={() => {
+              setDrawerOpen(true);
+            }}
+          >
+            Filter
+          </Button>
         </div>
 
         <Table
@@ -119,7 +132,58 @@ const Prospects = () => {
           loading={isLoading}
           bordered={true}
           scroll={{ x: 900 }}
+          className="mt-4"
         />
+        <Drawer
+          open={drawerOpen}
+          onClose={() => setDrawerOpen(false)}
+          size={"default"}
+          title="Filter Prospects"
+        >
+          <Form
+            name="modalFilter"
+            form={filterForm}
+            layout="vertical"
+            className="pt-8 px-4"
+            // onValuesChange={handleFilterValuesChange}
+            onFinish={handleFilter}
+          >
+              <Form.Item
+                label="Filter by Country"
+                name="countryFilter"
+                className="w-56"
+              >
+                <Select
+                  mode="multiple"
+                  loading={loadingCountries}
+                  options={
+                    countries?.map((country) => {
+                      return { value: country.id, label: country.country_name };
+                    }) || []
+                  }
+                />
+              </Form.Item>
+              <Form.Item
+                label="Filter by Program Type"
+                name="filterProgram"
+                className="w-56"
+              >
+                <Select
+                  mode="multiple"
+                  loading={loadingProgramTypes}
+                  options={
+                    allProgramTypes?.map((programType) => {
+                      return {
+                        value: programType.id,
+                        label: programType.program_name,
+                      };
+                    }) || []
+                  }
+                />
+              </Form.Item>
+            <AppButton label="Apply Filter" />
+          </Form>
+        </Drawer>
       </div>
     </>
   );
